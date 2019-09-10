1a90ec6b932fb2b02ce8e2fd2d81d3ad6eac6d82
tl <thl13@zips.uakron.edu>
2019-09-10 13:40:54 -0400

    Add empty function definitions

--- project1/project1part1/bigInt435/rsa435.cc
+++ project1/project1part1/bigInt435/rsa435.cc
@@ -1,5 +1,11 @@
 // You need to complete this program for a part of your first project.
 
+// When run, this program will:
+// 1) Generate two large prime numbers p and q
+// 2) Save p and q to a file named p_q.txt
+// 4) Generate encryption key pair (e, n) and decryption key pair (d, n) for n = p* q
+// 5) Save (e, n) and (d, n) to files name e_n.txt and d_n.txt, respectively
+
 // Standard libraries
 #include <string>
 #include <iostream>
@@ -9,6 +15,34 @@
 // 'BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
 
+BigUnsigned getPrime(int minBits) { // radnomly generate a large prime number with at least a certain number of bits
+   // get to minimum value
+   BigUnsigned p(1);
+   p <<= minBits;
+
+   // make random modification to value
+
+   // adjust until primality test passed
+
+   return p;
+}
+bool isPrime(BigUnsigned p) { // use Fermat test to check if a number is prime
+   //
+   return true;
+}
+
+BigUnsigned getE(BigUnsigned chi) { // generate encryption key
+   // given e as any odd integer with no common factors with chi (other than 1)
+   BigUnsigned e(1);
+   return e;
+}
+
+BigUnsigned getD(BigUnsigned e, BigUnsigned chi) { // get decryption key
+   // given d as the integer where de % chi = 1
+   BigUnsigned d(1);
+   return d;
+}
+
 int main(){
 	/* The library throws 'const char *' error messages when things go
 	 * wrong.  It's a good idea to catch them using a 'try' block like this
@@ -16,7 +50,7 @@ int main(){
 	 * code that uses exceptions. */
 	try {
 		      
-      std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
+      /*std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
       BigUnsigned big1 = BigUnsigned(1);
       for (int i=0;i<400;i++) {
          big1 = big1*10 +rand();
@@ -33,7 +67,19 @@ int main(){
       BigUnsigned big3 = big1*big2;
       std::cout <<big3;
       std::cout << "my big3/big2 !!!\n";
-      std::cout <<big3/big2;
+      std::cout <<big3/big2;*/
+
+      // get inital parameters
+      BigUnsigned p = getPrime(512); 
+      BigUnsigned q = getPrime(512);
+      
+      // save primes to file
+
+      // get key parameters
+      BigUnsigned n = p * q;
+      BigUnsigned chi = (p - 1) * (q - 1);
+
+      // save keys to file
       
 	} catch(char const* err) {
 		std::cout << "The library threw an exception:\n"
